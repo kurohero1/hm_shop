f3d6e51e19a6064a68c136741c8248658e721f3d
kurohero1 <tky2402108@stu.o-hara.ac.jp>
2026-01-14 17:22:23 +0900

    feat: load firebase config from runtime js instead of hardcoding

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,11 +1,32 @@
+import 'dart:js' as js;
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-const FirebaseOptions webFirebaseOptions = FirebaseOptions(
-  apiKey: 'AIzaSyBkKR-9DqfCqRXuaQgDe1yUg4rbft9Lrlk',
-  authDomain: 'sanpoai.firebaseapp.com',
-  projectId: 'sanpoai',
-  storageBucket: 'sanpoai.firebasestorage.app',
-  messagingSenderId: '655288395518',
-  appId: '1:655288395518:web:3e1180200c705e9c5ee7af',
-  measurementId: 'G-ZTXF6TBY1N',
-);
+FirebaseOptions get webFirebaseOptions {
+  if (!kIsWeb) {
+    throw StateError('webFirebaseOptions is only supported on web');
+  }
+
+  final dynamic config = js.context['__FIREBASE_CONFIG'];
+  if (config == null) {
+    throw StateError('__FIREBASE_CONFIG is not defined on window');
+  }
+
+  String _readString(String key) {
+    final value = config[key];
+    if (value is String && value.isNotEmpty) {
+      return value;
+    }
+    throw StateError('Missing or invalid Firebase config field: $key');
+  }
+
+  return FirebaseOptions(
+    apiKey: _readString('apiKey'),
+    authDomain: _readString('authDomain'),
+    projectId: _readString('projectId'),
+    storageBucket: _readString('storageBucket'),
+    messagingSenderId: _readString('messagingSenderId'),
+    appId: _readString('appId'),
+    measurementId: _readString('measurementId'),
+  );
+}
