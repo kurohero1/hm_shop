f94556604b8e2703d43cae31dd759a0b3de0ede5
kurohero1 <tky2402108@stu.o-hara.ac.jp>
2026-01-14 16:37:13 +0900

    fix: commit firebase_options.dart to resolve vercel deployment config error

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions webFirebaseOptions = FirebaseOptions(
+  apiKey: 'AIzaSyBkKR-9DqfCqRXuaQgDe1yUg4rbft9Lrlk',
+  authDomain: 'sanpoai.firebaseapp.com',
+  projectId: 'sanpoai',
+  storageBucket: 'sanpoai.firebasestorage.app',
+  messagingSenderId: '655288395518',
+  appId: '1:655288395518:web:3e1180200c705e9c5ee7af',
+  measurementId: 'G-ZTXF6TBY1N',
+);
