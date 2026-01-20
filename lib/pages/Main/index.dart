1e953da4bb6a2f1934fcbea1571a1b3f33fe638a
kurohero1 <tky2402108@stu.o-hara.ac.jp>
2026-01-20 13:53:15 +0900

    fix: update hardcoded version string to v1.0.3

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -43,7 +43,7 @@ class _MainPageState extends State<MainPage> {
   double? _waypointLon;
   String _systemComment = '';
   bool _filtersCollapsed = false;
-  final String _appVersion = 'v1.0.2'; // 版本号
+  final String _appVersion = 'v1.0.3'; // 版本号
 
   @override
   void initState() {
